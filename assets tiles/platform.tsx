4f7e92f9d856eeff377842bafbc0b87de92da8be
FabreVictor <Victor.fabre@lahiette.com>
2022-01-26 11:37:37 +0100

    Create platform.tsx

---
+++ assets tiles/platform.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="platform" tilewidth="32" tileheight="32" tilecount="12" columns="12">
+ <image source="../assets/platform.png" width="400" height="32"/>
+</tileset>
